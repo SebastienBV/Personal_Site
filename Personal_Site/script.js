491ecf31fff4ac34cf5f418d2aaf98e4732ce78d
SebastienBV <sebastienbv18@gmail.com>
2016-08-09 23:13:45 +0200

    Add files via upload

---
+++ Personal_Site/script.js
@@ -0,0 +1,25 @@
+//var yourName = prompt("What is your name?");
+
+//document.write (yourName)
+
+
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-78082850-1', 'auto');
+  ga('send', 'pageview');
+
+(function(p,u,s,h){
+    p._pcq=p._pcq||[];
+    p._pcq.push(['_currentTime',Date.now()]);
+    s=u.createElement('script');
+    s.type='text/javascript';
+    s.async=true;
+    s.src='https://cdn.pushcrew.com/js/52aa42b000a9b72cbff5e8d99aaf0d6a.js';
+    h=u.getElementsByTagName('script')[0];
+    h.parentNode.insertBefore(s,h);
+})(window,document);
